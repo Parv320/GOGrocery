c81e353c64b2dfa9d77613be6bd6085ecea349c1
Amandeep <amandeep5678kaur@gmail.com>
2020-10-26 15:23:27 -0400

    GoGetGrocery

---
+++ app/src/main/java/com/example/gogetgrocery/Caregories.java
@@ -0,0 +1,14 @@
+package com.example.gogetgrocery;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Caregories extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_caregories);
+    }
+}
